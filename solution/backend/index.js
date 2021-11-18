8c5a67f335c3b7ba1e1c96261891aa89ca5934d6
Rishi Sankar <rishi.sankar@gmail.com>
2021-11-18 10:43:23 -0800

    starter code

---
+++ solution/backend/index.js
@@ -0,0 +1,158 @@
+/*
+Initialization of Node.js / Express (you can ignore this)
+*/
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors');
+const app = express()
+const port = 8000
+app.use(cors());
+app.options('*', cors());
+app.use(bodyParser.json())
+
+/*
+Create variables to keep track of the game state:
+  - board_state is an array of 9 values representing the value at each square ("" / "X" / "O")
+  - winner represents whether someone has won the game yet ("" / "X" / "O")
+  - turn represents whose turn it is ("X" / "O")
+You will need to update these within the endpoints.
+*/
+let board_state = Array(9).fill("");
+let winner = "";
+let turn = "X"
+
+/*
+This function calculates whether someone has won the game
+given an array of sqauares (i.e. the board state).
+Returns "" (no winner yet), "X", or "O"
+*/
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return "";
+}
+
+/*
+
+Endpoint description (using JSON responses)
+
+1. GET /get_board_state (no params)
+Responds with game state:
+{
+  state: [...],
+  winner: "X" / "O" / "",
+  turn: "X" / "O" / ""
+}
+
+2. POST /play_move (int pos, string piece)
+Responds with whether the move was valid + new game state:
+{
+  success: true/false,
+  state: [...],
+  winner: "X" / "O" / "",
+  turn: "X" / "O" / ""
+}
+
+3. POST /restart
+Responds with the reset game state:
+{
+  state: [...],
+  winner: "X" / "O" / "",
+  turn: "X" / "O" / ""
+}
+
+*/
+
+/*
+With Express, this is how to add an endpoint (app.get is a GET endpoint).
+To send a JSON response, use:
+  res.json(
+    {
+      field1: val1,
+      field2: val2,
+      ...
+    }
+  )
+*/
+app.get('/get_board_state', (req, res) => {
+  res.json(
+    {
+      state: board_state,
+      winner: winner,
+      turn: turn
+    }
+  )
+})
+
+/*
+app.post corresponds to a POST endpoint.
+POST endpoints can come with a request body (used to specify parameters), accessed like:
+  let param1 = req.body.param1;
+For this endpoint, we have two parameters, pos and player.
+
+Things to do:
+1) Extract params from req.body (see above)
+2) If it is the player's turn, nobody has won yet, and the specified pos is 
+   empty / in array bounds, make the move (use if statements)
+3) To make the move, update board_state, recalculate the winner, update whose turn it is
+4) Send a JSON response with new game state. The success field should be true if the move was made.
+   (use res.json as in the previous endpoint)
+*/
+app.post('/play_move', function(req, res) {
+  let pos = req.body.pos;
+  let player = req.body.player;
+  let success = false;
+  if (player == turn && winner == "" && pos >= 0 && pos < 9 && board_state[pos] == "") {
+    success = true;
+    board_state[pos] = player;
+    winner = calculateWinner(board_state);
+    if (turn == "X") {
+      turn = "O";
+    } else {
+      turn = "X";
+    }
+  }
+  res.json({
+    success: success,
+    state: board_state,
+    winner: winner,
+    turn: turn
+  })
+});
+
+/*
+The final endpoint should reset the game state to restart the game.
+
+Things to do:
+1) Reset board_state, winner, turn to initial values
+2) Send a JSON response with the resetted game state
+*/
+app.post('/restart', function(req, res) {
+  board_state = Array(9).fill("");
+  winner = "";
+  turn = "X"
+  res.json({
+    state: board_state,
+    winner: winner,
+    turn: turn
+  })
+});
+
+// After all the endpoints, start up the backend server! (can ignore this)
+app.listen(port, () => {
+  console.log(`Backend listening at http://localhost:${port}`)
+})
---
+++ startercode/frontend/precache-manifest.106329b0b1da4eb4d46a1954eea581bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38a3b39fc2758592d1ba309408358c70",
+    "url": "./index.html"
+  },
+  {
+    "revision": "02674212b239c0bc3428",
+    "url": "./static/css/main.29f37d4b.chunk.css"
+  },
+  {
+    "revision": "a34c6aeda3a22d5ca9ab",
+    "url": "./static/js/2.3ada8c99.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "./static/js/2.3ada8c99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02674212b239c0bc3428",
+    "url": "./static/js/main.85a89fec.chunk.js"
+  },
+  {
+    "revision": "8f80d4c4fc437fe9c538",
+    "url": "./static/js/runtime-main.11557d18.js"
+  }
+]);
